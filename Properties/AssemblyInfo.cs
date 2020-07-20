e26de5b676d2cdb04b97f6ac68344aa04d6b48dd
Evan Taylor <taylorevan113@gmail.com>
2020-07-20 08:45:32 -0500

    Cleaned up Squirrel stuff, don't need it for this type of app

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WVA_Scan_Installer_Service")]
+[assembly: AssemblyTitle("WVA Scan Installer Service")]
 [assembly: AssemblyDescription("WVA Scan Installer Service - A lightweight mass installer app for WVA Scan")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wisconsin Vision Associates")]
 [assembly: AssemblyProduct("WVA Scan Installer Service")]
-[assembly: AssemblyCopyright("Copyright � Wisconsin Vision Associates 2020")]
+[assembly: AssemblyCopyright("Copyright © Wisconsin Vision Associates 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
